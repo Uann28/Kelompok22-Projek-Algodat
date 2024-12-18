d6658e344b78bd95589c72e5aebdc6879f4dfb42
MoonForLight <f1d02310120@student.unram.ac.id>
2024-12-18 21:46:27 +0800

    Update BubbleSort dan Void Battle

--- Traversal.java
+++ Traversal.java
@@ -100,4 +100,64 @@ class Traversal {
         }
         System.out.println();
     }
+
+    void bubbleSortVillainsInCity(Kota kota) {
+        if (kota.headVillain == null || kota.headVillain.next == null) {
+            return;
+        }
+
+        boolean swapped;
+        do {
+            swapped = false;
+            Villain current = kota.headVillain;
+            Villain prev = null;
+            while (current != null && current.next != null) {
+                Villain next = current.next;
+                if (current.kekuatan > next.kekuatan) {
+                    swapped = true;
+                    if (prev == null) {
+                        kota.headVillain = next;
+                    } else {
+                        prev.next = next;
+                    }
+                    current.next = next.next;
+                    next.next = current;
+                    prev = next;
+                } else {
+                    prev = current;
+                    current = next;
+                }
+            }
+        } while (swapped);
+    }
+    
+    void battle(Villain villain) {
+        while (moonKnight.hp > 0 && villain.HP > 0) {
+            System.out.println("\nHP Moon Knight: " + moonKnight.hp);
+            System.out.println("HP " + villain.nama + " : " + villain.HP);
+            System.out.print("Tekan 'f' untuk menyerang atau 'd' untuk bertahan: ");
+            String input = scanner.nextLine().toLowerCase();
+            
+            if (input.equals("f")) {
+                villain.HP -= moonKnight.attackPower;
+                System.out.println("Moon Knight menyerang " + villain.nama + " sebesar " + moonKnight.attackPower + " damage!");
+                if (villain.HP > 0) {
+                    moonKnight.hp -= villain.kekuatan;
+                    System.out.println(villain.nama + " menyerang balik sebesar " + villain.kekuatan + " damage!");
+                    System.out.println("HP Moon Knight berkurang sebesar " + villain.kekuatan + ". Sisa HP: " + moonKnight.hp);
+                } else {
+                    System.out.println(villain.nama + " telah dikalahkan!");
+                }
+            } else if (input.equals("d")) {
+                System.out.println(villain.nama + " menyerang tapi tidak berpengaruh karena " + moonKnight.nama + " menangkis serangannya!!!");
+            } else {
+                System.out.println("Input tidak valid! Silakan tekan 'f' atau 'd' !");
+            }
+
+            if (moonKnight.hp <= 0) {
+                System.out.println("\n" + moonKnight.nama + " telah dikalahkan! Game over!!!");
+                return;
+            }
+        }
+    }
 }
