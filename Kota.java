4cd1dc2dd5db874d9c70dee24b6c45595f3b3139
Madani022 <madaniahmad599@gmail.com>
2024-12-21 21:45:09 +0800

    Update class kota

--- Kota.java
+++ Kota.java
@@ -1,14 +1,16 @@
-class Kota {
+class Kota { 
     String nama;
     Villain headVillain;
-    Kota next;
     String faseBulan;
+    Tetangga headTetangga;
+    Kota next;
 
     Kota(String nama, String faseBulan) {
         this.nama = nama;
         this.headVillain = null;
-        this.next = null;
         this.faseBulan = faseBulan;
+        this.headTetangga = headTetangga;
+        this.next = null;
     }
 
     void tambahVillain(Villain villain) {
@@ -22,4 +24,17 @@ class Kota {
             current.next = villain;
         }
     }
-}
+
+    void tambahTetangga(Kota tetangga, int bobot) {
+        Tetangga newTetangga = new Tetangga(tetangga, bobot);
+        if (headTetangga == null) {
+            headTetangga = newTetangga;
+        } else {
+            Tetangga current = headTetangga;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newTetangga;
+        }
+    }
+} 
